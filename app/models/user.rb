412bb419156025b1f4278232530051df52d1c573
Josemar Varela <josemarvarela.gremio@gmail.com>
2019-11-11 21:47:46 -0300

    testes ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
     validates :login, presence: true
+
+    before_validation :determine_login
+
+    def generate_login
+        return self.name.downcase
+    end
+
+    def determine_login
+        self.login = self.generate_login
+    end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,19 +1,34 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
   test "valida login obrigatorio" do
     u = User.new
     u.name = "Carlos alberto"
-    u.password = "123456"
+    u.password = "123456456"
     u.login = ""
-    assert_not u.valid?
+    assert u.valid?
   end
 
-  test "valida usuario com login" do
+  test "valida usuario com login informado" do
     u = User.new
     u.name = "Carlos alberto"
     u.password = "123456"
     u.login = "carlos"
     assert u.valid?
+  end
+
+  test "valida geracao login" do
+    u = User.new
+    u.name = "josEMar"    
+    assert_equal u.generate_login, "josemar"
+  end
+
+  test "valida se determina o login na validacao" do
+    u = User.new
+    u.name = "Josemar"
+    u.password = "pwd"
+    assert u.valid?
+    assert_equal u.login, "josemar"
   end
 end
